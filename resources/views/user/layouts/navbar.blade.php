97e47ec0cd32ffe31902a097cb53b3035efc3a3c
Lucky7Tb <Luckytri101@gmail.com>
2021-12-05 20:49:12 +0700

    fix bug

--- resources/views/user/layouts/navbar.blade.php
+++ resources/views/user/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar sm:px-44 shadow-lg bg-primary text-neutral-content z-10">
 	<div class="flex-none px-2 mx-2">
 		<span class="text-lg font-bold">
-			Mentorku
+			Hidden Food
 		</span>
 	</div>
 	<div class="flex-1 px-2 mx-2">
